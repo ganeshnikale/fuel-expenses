995d6a816ada9a789209939f056898aa4b828167
ganesh nikale <ganesh.nikale@digitas.com>
2022-10-15 01:22:13 +0530

    fuel expences init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+import { useState} from 'react';
 import './App.css';
 
+
 function App() {
+
+    const fuleExpS = {requoredFuel: 0, fuelCost:0, fuelRate:0};
+
+  const [fuleExp, setFuelExp] = useState(fuleExpS);
+
+  const CalculateExp = (e) => {
+    e.preventDefault();
+   
+    let estDistance = e.target.estDistance.value;
+    let expAvg = e.target.expAvg.value;
+    let expAvgFuelCost = Number(e.target.expAvgFuelCost.value);
+
+
+    let requiredFuel = (estDistance/expAvg);
+
+    let fuelCost = requiredFuel*expAvgFuelCost;
+    
+    const fuel = () => {
+      return {...fuleExp, fuelRate:expAvgFuelCost, requoredFuel: requiredFuel, fuelCost: fuelCost};
+    };
+
+
+     setFuelExp(fuel());
+
+     console.log(fuleExp);
+
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h2>Get fuel Expenses for your trip</h2>
+          </div>
+        </div>
+        <form onSubmit={CalculateExp}>
+          <div className='row'>
+
+            <div className="col-md-6">
+              <div className="form-control">
+                <label> Enter Estimated Distance</label>
+                <input type="text" name="estDistance"/>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="form-control">
+                <label>your expected avarage</label>
+                <input type="text" name="expAvg"/>
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div className="form-control">
+                <label>your expected avarage fuel cost</label>
+                <input type="text" name="expAvgFuelCost"/>
+              </div>
+            </div>
+
+
+            <div className="col-md-12">
+              <button className="btn btn-primary" > calculate</button>
+            </div>
+          </div>
+          </form>
+
+          <div className='row'>
+            <div className='col-md-12'>
+            
+            <h4> you required total fuel to complete this Journey is<br/> {fuleExp.requoredFuel.toFixed(2)} ltr your fuel cost {fuleExp.fuelCost.toFixed(2)}</h4>
+          
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
